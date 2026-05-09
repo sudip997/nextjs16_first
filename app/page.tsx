9b51f63ea99146aa29868e1042a6732f1113d624
sudip <sudippatil008@gmail.com>
2026-05-09 19:39:21 +0530

    comply with v16 gods

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import { IEvent } from "@/database";
+// import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
-// import events from "@/lib/constants";
+import events from "@/lib/constants";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
   "use cache";
   cacheLife("hours");
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  //   const response = await fetch(`${BASE_URL}/api/events`);
+  //   const { events } = await response.json();
 
   return (
     <section>
@@ -27,7 +27,7 @@ const page = async () => {
         <ul className="events">
           {events &&
             events.length > 0 &&
-            events.map((event: IEvent) => (
+            events.map((event) => (
               <li key={event.title}>
                 <EventCard {...event} />
               </li>
